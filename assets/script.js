1c87c799214ea139caaa90cf4b6436d1b43755b0
paslian <paslianp5@gmail.com>
2024-07-27 22:30:43 +0700

    update gallery section

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,40 @@ document.addEventListener("DOMContentLoaded", function () {
     .move(null, { to: "END" })
     .go();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let sections = document.querySelectorAll("section");
+  let currentSection = 0;
+  let isScrolling = false;
+
+  function scrollToSection(index) {
+    sections[index].scrollIntoView({ behavior: "smooth" });
+    isScrolling = true;
+    setTimeout(() => {
+      isScrolling = false;
+    }, 500);
+  }
+
+  function handleScroll(event) {
+    if (isScrolling) return;
+    event.preventDefault();
+    let delta = Math.sign(event.deltaY);
+    currentSection = Math.min(
+      Math.max(currentSection + delta, 0),
+      sections.length - 1
+    );
+    scrollToSection(currentSection);
+  }
+
+  window.addEventListener("wheel", handleScroll);
+  window.addEventListener("keydown", function (event) {
+    if (isScrolling) return;
+
+    if (event.key === "ArrowDown") {
+      currentSection = Math.min(currentSection + 1, sections.length - 1);
+    } else if (event.key === "ArrowUp") {
+      currentSection = Math.max(currentSection - 1, 0);
+    }
+    scrollToSection(currentSection);
+  });
+});
